bab5b745048f007f75ee22be9d754527135fca73
Dennis Van Ee <dennisva@boilerconnect.com>
2014-10-04 22:15:16 -0600

    Add hostgroup.add_host to add a host to a specific group

--- lib/zabbixapi/classes/hostgroups.rb
+++ lib/zabbixapi/classes/hostgroups.rb
@@ -16,5 +16,23 @@ class ZabbixApi
     def key
       "groupid"
     end
+    
+        # Update hostgroup, add host to group
+    # 
+    # * *Args*    :
+    #   - +data+ -> Hash with :groupid => id, :hostids => []
+    # * *Returns* :
+    #   - Integer
+    def add_host(data)
+      result = @client.api_request(
+        :method => "hostgroup.massAdd", 
+        :params => {
+          :groups => [{:groupid => data[:groupid]}],
+          :hosts => data[:hostids].map { |h| {:hostid => h} }
+        }
+      )
+      result ? result['groupids'][0].to_i : nil
+    end
+    
   end
 end
