54ea59741b9ec32899f09686094dfdeb2f804223
Dennis Van Ee <dennisva@boilerconnect.com>
2014-10-11 16:12:04 -0600

    Add update_medias to users class

--- lib/zabbixapi/classes/users.rb
+++ lib/zabbixapi/classes/users.rb
@@ -42,6 +42,24 @@ class ZabbixApi
       )
       result ? result : nil
     end
-
+    
+    # replace a users media with the provided media. This just blows away existing media.
+    # data[:userid] => userid
+    # data[:medias] => [ {:mediatypeid => 1, :sendto => "support@company.com", :active => 0, :severity => 0x3C,
+    #                   :period => "1-7,00:00-23:59;"}, {:mediatypeid => 2, :sendto => "13067138956",
+    #                   :active => 0, :severity => 0x30, :period => "1-7,00:00-23:59;"} ]
+    # :active, 0 is enabled
+    # :severity, bit mask MSB to LSB (Disaster|High|Average|Warning|Info|None)
+    def update_medias(data)
+      result = @client.api_request(
+        :method => "user.updatemedia",
+        :params => {
+          :users => { :userid => data[:userid] },
+          :medias => data[:medias],
+          :output => "extend"
+        }
+      )
+      result ? result : nil
+    end
   end
 end
